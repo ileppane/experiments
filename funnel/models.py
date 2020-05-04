a22749dd3629f99c0fddf89f11a69564e54cd3ee
Ilkka Leppänen <ilkkaleppanen@Ilkkas-MacBook-Air.local>
2020-05-04 11:32:17 +0100

    funnel final steps

--- funnel/models.py
+++ funnel/models.py
@@ -20,7 +20,7 @@ Deming''s funnel expreriment for OM students
 class Constants(BaseConstants):
     name_in_url = 'funnel'
     players_per_group = None
-    num_rounds = 30
+    num_rounds = 40
     instructions_template = 'funnel/Instructions.html'
     stdev = 30 # standard deviation for marble location
 
@@ -35,8 +35,8 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 
-    x = models.IntegerField(min=-150, max=150)
-    y = models.IntegerField(min=-150, max=150)
+    x = models.IntegerField()
+    y = models.IntegerField()
     xcoord = models.IntegerField()
     ycoord = models.IntegerField()
     score = models.FloatField()
--- funnel/pages.py
+++ funnel/pages.py
@@ -81,7 +81,7 @@ class MyPage(Page):
 class Results(Page):
 
     def is_displayed(self):
-        return self.round_number == 10 or self.round_number == 20 or self.round_number == 30 #Constants.num_rounds
+        return self.round_number == 10 or self.round_number == 20 or self.round_number == 30 or self.round_number == 40#Constants.num_rounds
 
     def vars_for_template(self):
 
@@ -90,30 +90,52 @@ class Results(Page):
         allxcoord = []
 
         cumulativescore = 0
+        controllimitbreach = 0 # counts how many times UCL or LCL is exceeded
         i = 1
         while i < self.round_number + 1:
             allxcoord.append(self.player.in_round(i).xcoord)
             allycoord.append(self.player.in_round(i).ycoord)
             cumulativescore += self.player.in_round(i).score
+            if abs(self.player.in_round(i).xcoord) > 3*Constants.stdev:
+                controllimitbreach += 1
+            if abs(self.player.in_round(i).ycoord) > 3*Constants.stdev:
+                controllimitbreach += 1
             i += 1
+
+        if controllimitbreach > 3:
+            controllimitbreachtext = "You have exceeded the control limits " + str(controllimitbreach) + " times. Try to be more careful in the future rounds."
+            if self.round_number == Constants.num_rounds:
+                controllimitbreachtext = "You have exceeded the control limits " + str(controllimitbreach) + " times."
+        elif controllimitbreach > 0:
+            controllimitbreachtext = "You have exceeded the control limits only " + str(controllimitbreach) + " times. Good job."
+        else:
+            controllimitbreachtext = "You have not exceeded the control limits even once. Excellent job!"
 
         # lowest score simulation, assuming funnel is kept at centre
         mclowscore = []
         for mc in range(1000):
             lowscore = 0
-            for i in range(Constants.num_rounds):
+            for i in range(self.round_number):
                 xcoord = numpy.ceil(numpy.random.normal(0, Constants.stdev))
                 ycoord = numpy.ceil(numpy.random.normal(0, Constants.stdev))
                 lowscore += math.sqrt(xcoord * xcoord + ycoord * ycoord)
 
             mclowscore.append(lowscore)
 
+        if min(mclowscore) < .8*cumulativescore:
+            lowscoretext = "You can do much better."
+        else:
+            lowscoretext = "Great job."
+
         return {
             'allxcoord': allxcoord,
             'allycoord': allycoord,
             'round': self.player.round_number,
             'lowscore': min(mclowscore),
-            'cumulativescore': cumulativescore
+            'lowscoretext': lowscoretext,
+            'cumulativescore': cumulativescore,
+            'controllimitbreach': controllimitbreach,
+            'controllimitbreachtext': controllimitbreachtext
         }
 
 page_sequence = [InitialPage, MyPage, Results]
