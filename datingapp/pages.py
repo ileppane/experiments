6e665f3bd98708d0d0878c2d23d6e2d373291f35
Ilkka Leppänen <ilkkaleppanen@Ilkkas-MacBook-Air.local>
2020-05-12 19:40:56 +0100

    ba challenge updates

--- datingapp/pages.py
+++ datingapp/pages.py
@@ -5,8 +5,7 @@ from .models import Constants
 
 class MyPage(Page):
     form_model = 'player'
-    form_fields = ['Jack','Thomas','James','Joshua','Daniel','Harry','Samuel','Joseph','Matthew','Callum']
-
+    form_fields = ['Chloe','Emily','Megan','Charlotte','Jessica','Lauren','Sophie','Olivia','Hannah','Lucy']
 
 class Results(Page):
 
@@ -14,37 +13,36 @@ class Results(Page):
 
         correct = ""
         points = 0
-        if (self.player.Jack == "Emily"):
+        if (self.player.Emily == "Jack"):
             correct = correct + "Jack with Emily\n "
             points = points + 1
-        if (self.player.Thomas == "Megan"):
+        if (self.player.Megan == "Thomas"):
             correct = correct + "Thomas with Megan\n"
             points = points + 1
-        if (self.player.James == "Olivia"):
+        if (self.player.Olivia == "James"):
             correct = correct + "James with Olivia\n"
             points = points + 1
-        if (self.player.Joshua == "Lucy"):
+        if (self.player.Lucy == "Joshua"):
             correct = correct + "Joshua with Lucy\n"
             points = points + 1
-        if (self.player.Daniel == "Sophie"):
+        if (self.player.Sophie == "Daniel"):
             correct = correct + "Daniel with Sophie\n"
             points = points + 1
-        if (self.player.Harry == "Chloe"):
+        if (self.player.Chloe == "Harry"):
             correct = correct + "Harry with Chloe\n"
             points = points + 1
-        if (self.player.Samuel == "Jessica"):
+        if (self.player.Jessica == "Samuel"):
             correct = correct + "Samuel with Jessica\n"
             points = points + 1
-        if (self.player.Joseph == "Hannah"):
+        if (self.player.Hannah == "Joseph"):
             correct = correct + "Joseph with Hannah\n"
             points = points + 1
-        if (self.player.Matthew == "Lauren"):
+        if (self.player.Lauren == "Matthew"):
             correct = correct + "Matthew with Lauren\n"
             points = points + 1
-        if (self.player.Callum == "Charlotte"):
+        if (self.player.Charlotte == "Callum"):
             correct = correct + "Callum with Charlotte\n"
             points = points + 1
-
         if (correct == ""):
             correct = "You did not have any correct matches."
 
